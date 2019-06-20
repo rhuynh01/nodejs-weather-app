c71d77e506a6793f39c19d963463d6f22f4e6be3
Rodney Huynh <rhuynh2005@gmail.com>
2019-06-20 17:23:20 -0500

    add additional fiels in the forecase callback function

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
+const sunriseTime = document.querySelector('#sunriseTime')
+const sunsetTime = document.querySelector('#sunsetTime')
 
 messageOne.textContent = 'Loading...'
 messageTwo.textContent = ''
@@ -13,11 +15,15 @@ weatherForm.addEventListener("submit", e => {
 
     fetch("/weather?address=" + location).then(response => {
         response.json().then(data => {
+            // console.log(data)
             if (data.error) {
                 messageOne.textContent = data.error
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
+                // sunriseTime.textContent = data.forecast['sunriseTime']
+                // sunsetTime.textContent = data.forecast['sunsetTime']
+                
             }
         });
     });
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,15 @@ const forecast = (longitute, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            // const currentlyStatus =  body.daily.data[0].summary + ' It is currently ' +             body.currently.temperature + ' degress out. There is a ' +                          body.currently.precipProbability + '% chance of rain.'
+            // const sunriseTime = body.daily.data[0].sunriseTime
+            // const sunsetTime = body.daily.data[0].sunsetTime
+            // callback(undefined, { 
+            //     currentlyStatus, 
+            //     sunriseTime, 
+            //     sunsetTime
+            // })
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' +             body.currently.temperature + ' degress out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '.  There is a ' + body.currently.precipProbability + '% chance of rain.' )
         }
     })
 }
